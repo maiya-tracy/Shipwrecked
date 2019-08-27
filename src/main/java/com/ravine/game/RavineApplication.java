ceaf5d0b6e0fad3fe8c03c36b91ddbacccf5038b
Maiya Tracy <maiya.tracy@gmail.com>
2019-08-27 11:25:00 -0700

    empty project

---
+++ src/main/java/com/ravine/game/RavineApplication.java
@@ -0,0 +1,13 @@
+package com.ravine.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavineApplication.class, args);
+	}
+
+}
