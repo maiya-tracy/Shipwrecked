4fe3d48a131cac8fc285ff168c5e6cf7d1b0ebf3
Maiya Tracy <maiya.tracy@gmail.com>
2019-08-27 14:53:57 -0700

    rename to shipwrecked

--- src/main/java/com/shipwrecked/game/ServletInitializer.java
+++ src/main/java/com/shipwrecked/game/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.ravine.game;
+package com.shipwrecked.game;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(RavineApplication.class);
+		return application.sources(ShipwreckedApplication.class);
 	}
 
 }
