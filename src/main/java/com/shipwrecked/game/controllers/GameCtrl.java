93c3b0c2000ccab53d172fbc5ab1fcefa12e309a
Maiya Tracy <maiya.tracy@gmail.com>
2019-08-27 17:44:17 -0700

    routing

--- src/main/java/com/shipwrecked/game/controllers/GameCtrl.java
+++ src/main/java/com/shipwrecked/game/controllers/GameCtrl.java
@@ -2,6 +2,7 @@ package com.shipwrecked.game.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class GameCtrl {
@@ -12,5 +13,13 @@ public class GameCtrl {
 	@GetMapping("/getShipwrecked")
 	public String createOrJoinPage() {
 		return "game/createOrJoinPage.jsp";
+	}
+	@GetMapping("/getShipwrecked/{game_id}")
+	public String lobby(@PathVariable("game_id") Long game_id) {
+		return "game/lobby.jsp";
+	}
+	@GetMapping("/getShipwrecked/{game_id}/go")
+	public String inGame(@PathVariable("game_id") Long game_id) {
+		return "game/inGame.jsp";
 	}
 }
